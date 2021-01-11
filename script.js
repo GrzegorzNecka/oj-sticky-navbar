b1e9cd8639f0e7bbb45465799f6fc186e93543f4
GrzegorzNecka <strzypior@gmail.com>
2021-01-11 16:18:36 +0100

    1 file modified

--- script.js
+++ script.js
@@ -9,7 +9,10 @@
 window.addEventListener("DOMContentLoaded", () => {
   const navbar = document.querySelector("header.navbar");
   const navbarHeight = navbar.getBoundingClientRect().height;
+  const hero = document.querySelector(".hero");
 
+
+console.log(hero)
   window.addEventListener("scroll", e => {
     const navbarTop = navbar.getBoundingClientRect().top;
 
