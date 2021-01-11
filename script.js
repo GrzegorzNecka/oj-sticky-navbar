d26b2f2ef6efeb128c944b92809ef5a91b57f40a
GrzegorzNecka <strzypior@gmail.com>
2021-01-11 16:06:54 +0100

    2 files modified

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-console.log("Working");
+window.addEventListener("load", () => {
+  document.querySelector("header.navbar");
+});
 
+window.addEventListener("DOMContentLoaded", () => {
+  const navbar = document.querySelector("header.navbar");
+  const navbarTop = navbar.getBoundingClientRect().top;
+  const navbarHeight = navbar.getBoundingClientRect().height;
 
-const navbar = document.querySelector('navbar')
+  window.addEventListener("scroll", e => {
+    console.log("window", this.scrollY);
+
+    console.log("nav", navbarTop - navbarHeight);
+
+    if ((navbar.getBoundingClientRect().top = 0)) {
+      console.log("a");
+    }
+  });
+});
